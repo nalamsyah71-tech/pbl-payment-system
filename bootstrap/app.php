5bf63f61abf76c072184fbf5e8c80fe590fc2eca
nalamsyah71-tech <nalamsyah71@gmail.com>
2026-06-05 13:47:33 +0700

    Register role middleware alias in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Tambahkan alias middleware role
+        $middleware->alias([
+            'role' => \App\Http\Middleware\CheckRole::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
